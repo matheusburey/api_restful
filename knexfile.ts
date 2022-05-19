dfcab5d8e7eb94f6efcb701e86a90fc869a5106e
matheusburey <matheushburei@gmail.com>
2022-05-19 18:08:20 -0300

    feat: config knex connection

--- knexfile.ts
+++ knexfile.ts
@@ -1,9 +1,14 @@
-module.exports = {
-  development: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: `${__dirname}/src/database/migrations`,
-    },
+import dotenv from "dotenv";
+import type { Knex } from "knex";
+
+dotenv.config()
+
+const config: Knex.Config = {
+  client: "pg",
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    directory: `${__dirname}/src/database/migrations`,
   },
 };
+
+export default config;
