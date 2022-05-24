fb6e247a946d32dcd1f5166c973227f1a0e5d84c
matheusburey <matheushburei@gmail.com>
2022-05-24 10:52:23 -0300

    feat: created route documentation swagger

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import cors from "cors";
 import express, { json } from "express";
+import swaggerUI from "swagger-ui-express";
 
 import router from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
@@ -9,5 +11,7 @@ app.use(cors());
 app.use(json());
 
 app.use("/api/v1", router);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 export default app;
