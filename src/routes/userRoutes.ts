ee918e180c22b5b370984cb642b4675d176e639b
matheusburey <matheushburei@gmail.com>
2022-05-24 10:53:53 -0300

    feat: added middleware

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,20 +1,30 @@
 import { Router } from "express";
 
+import { validateMiddleware } from "../middlewares/validateMiddleware";
 import CreateUsersController from "../modules/user/useCases/createUser/createUserController";
-import DeleteAllUsersController from "../modules/user/useCases/delteAllUsers/deleteAllUsersController";
-import DeleteUserByIdController from "../modules/user/useCases/delteUserById/deleteUserByIdController";
+import DeleteAllUsersController from "../modules/user/useCases/deleteAllUsers/deleteAllUsersController";
+import DeleteUserByIdController from "../modules/user/useCases/deleteUserById/deleteUserByIdController";
 import GetAllUsersController from "../modules/user/useCases/getAllUsers/getAllUsersController";
-import GetUserByIsController from "../modules/user/useCases/getUserById/getUserByIdController";
+import GetUserByIdController from "../modules/user/useCases/getUserById/getUserByIdController";
 import UpdateUserController from "../modules/user/useCases/updateUser/updateUserController";
+import { userFieldsSchemas } from "../schemas/userFieldsSchemas";
 
 const userRouter = Router();
 
 userRouter.get("", new GetAllUsersController().handle);
-userRouter.get("/:id", new GetUserByIsController().handle);
+userRouter.get("/:id", new GetUserByIdController().handle);
 
-userRouter.post("", new CreateUsersController().handle);
+userRouter.post(
+  "",
+  validateMiddleware(userFieldsSchemas),
+  new CreateUsersController().handle
+);
 
-userRouter.put("/:id", new UpdateUserController().handle);
+userRouter.put(
+  "/:id",
+  validateMiddleware(userFieldsSchemas),
+  new UpdateUserController().handle
+);
 
 userRouter.delete("", new DeleteAllUsersController().handle);
 userRouter.delete("/:id", new DeleteUserByIdController().handle);
