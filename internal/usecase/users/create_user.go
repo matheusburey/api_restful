a61d06507006c32015d63de49cc87250a5f236d8
matheusburey <matheushburei.99@gmail.com>
2025-04-24 21:19:18 -0300

    feat: create user case to valid json to create new user

---
+++ internal/usecase/users/create_user.go
@@ -0,0 +1,23 @@
+package users
+
+import (
+	"context"
+
+	"github.com/matheusburey/api_restful/internal/utils"
+)
+
+type CreateUserReq struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+func (req CreateUserReq) Valid(ctx context.Context) utils.Evaluator {
+	var eval utils.Evaluator
+
+	eval.CheckField(utils.NotBlank(req.Name), "name", "name is required")
+	eval.CheckField(utils.MinLength(req.Name, 5) && utils.MaxLength(req.Name, 100), "name", "min length is 5 and max length is 100")
+	eval.CheckField(utils.NotBlank(req.Email), "email", "email is required")
+	eval.CheckField(utils.MinLength(req.Email, 10) && utils.MaxLength(req.Email, 255), "email", "min length is 10 and max length is 255")
+
+	return eval
+}
